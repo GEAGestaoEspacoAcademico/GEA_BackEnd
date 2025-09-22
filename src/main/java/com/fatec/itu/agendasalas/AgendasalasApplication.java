e8ba6cabd5a9e7ac4228c77cd908755a0ba92c80
LucasMorais42 <lucasmorais2214@gmail.com>
2025-09-22 10:50:44 -0300

    Envio arquitetura de pastas

---
+++ src/main/java/com/fatec/itu/agendasalas/AgendasalasApplication.java
@@ -0,0 +1,13 @@
+package com.fatec.itu.agendasalas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendasalasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendasalasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fatec/itu/agendasalas/AgendasalasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.itu.agendasalas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendasalasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
