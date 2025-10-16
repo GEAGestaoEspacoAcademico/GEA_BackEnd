2fb5ce68bdb1b4ba6c1ae027181a27131f020082
bibiritriz <ntenhodoritosmasdepende@gmail.com>
2025-10-16 16:47:29 -0300

    :sparkles: feat(entity): add recurso and recursoSala entity (closes #17)

---
+++ src/main/java/com/fatec/itu/agendasalas/entity/RecursoSala.java
@@ -0,0 +1,46 @@
+package com.fatec.itu.agendasalas.entity;
+
+import java.io.Serializable;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Table(name = "RECURSOSSALAS")
+@Entity
+@Getter
+@Setter
+public class RecursoSala implements Serializable {
+  private static final long serialVersionUID = 1L;
+
+  public RecursoSala(Long idRecurso, Long idSala) {
+    this.idRecurso = idRecurso;
+    this.idSala = idSala;
+  }
+
+  @EqualsAndHashCode.Include
+  @Id
+  @Column(name = "id_recurso", insertable = false, updatable = false)
+  private Long idRecurso;
+
+  @EqualsAndHashCode.Include
+  @Id
+  @Column(name = "id_sala", insertable = false, updatable = false)
+  private Long idSala;
+
+  @ManyToOne
+  @JoinColumn(name = "id_sala")
+  private Sala sala;
+
+  @ManyToOne
+  @JoinColumn(name = "id_recurso")
+  private Recurso recurso;
+
+  @Column(name = "quantidade", nullable = false)
+  private int quantidade;
+}
