110eb404ef4406d5e6fe1d18903836d3a3ddebb6
Carvalh0Henrique <carvalhoghenrique@gmail.com>
2025-10-17 12:37:06 -0300

    feat(data.sql): Implementa a iniciação automática dos dados ao criar o banco em memória

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,8 @@
 INSERT INTO CARGOS(nome)
 VALUES ('USER'), ('ADMIN'), ('PROFESSOR');
 
-INSERT INTO USUARIOS(nome, login, senha, email, cargo_id) 
-VALUES ('Lucas', '1931', '123123', 'lucasmorais@122.com', '1');
+INSERT INTO USUARIOS (DTYPE, nome, login, senha, email, cargo_id)
+VALUES ('Usuario', 'Lucas', '1931', '123123', 'lucasmorais@1022.com', 1);
 
+INSERT INTO USUARIOS (DTYPE, nome, login, senha, email, cargo_id, registro_professor)
+VALUES ('PROFESSORES', 'Dimas', '1932', '1234', 'dimas@exemplo.com', 3, 1010);
