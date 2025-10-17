26db8197b307609fc7a316dc14875b252edb6d13
Henrique A. R. <henrique4r.it@gmail.com>
2025-10-16 23:49:43 -0300

    feat(controller): adiciona suporte a @CrossOrigin para habilitar CORS nos controller de Curso

--- src/main/java/com/fatec/itu/agendasalas/controllers/CursoController.java
+++ src/main/java/com/fatec/itu/agendasalas/controllers/CursoController.java
@@ -3,6 +3,7 @@ package com.fatec.itu.agendasalas.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.fatec.itu.agendasalas.entity.Curso;
 import com.fatec.itu.agendasalas.services.CursoService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("cursos")
 public class CursoController {
