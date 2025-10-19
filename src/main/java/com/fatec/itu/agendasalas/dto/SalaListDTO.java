fa8184273cfc7811d32b97500acd1c4cd04082a5
bibiritriz <ntenhodoritosmasdepende@gmail.com>
2025-10-19 19:50:21 -0300

    :sparkles: feat(controller, repository, service): add sala controller, repository and service (closes # 26)

---
+++ src/main/java/com/fatec/itu/agendasalas/dto/SalaListDTO.java
@@ -0,0 +1,4 @@
+package com.fatec.itu.agendasalas.dto;
+
+public record SalaListDTO(Long id, String nome, int capacidade, boolean disponibilidade) {
+}
--- src/main/java/com/fatec/itu/agendasalas/repositories/SalaRepository.java
+++ src/main/java/com/fatec/itu/agendasalas/repositories/SalaRepository.java
@@ -1,5 +1,6 @@
 package com.fatec.itu.agendasalas.repositories;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +8,5 @@ import com.fatec.itu.agendasalas.entity.Sala;
 
 @Repository
 public interface SalaRepository extends JpaRepository<Sala, Long> {
+  public List<Sala> findByDisponibilidade(boolean disponibilidade);
 }
