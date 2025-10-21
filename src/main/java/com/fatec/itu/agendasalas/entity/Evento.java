5c63baea0762b3b5d26aa7e6f6307da7dba04a8f
LucasMorais42 <lucasmorais2214@gmail.com>
2025-10-20 23:30:33 -0300

    testando

--- src/main/java/com/fatec/itu/agendasalas/entity/Evento.java
+++ src/main/java/com/fatec/itu/agendasalas/entity/Evento.java
@@ -1,7 +1,7 @@
 package com.fatec.itu.agendasalas.entity;
 
 import java.io.Serializable;
-
+/
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
