55f1f3105ef54866fedf4fae1ee2891b3a7a46d8
Henrique A. R. <henrique4r.it@gmail.com>
2025-10-22 23:52:07 -0300

    feat(AuthService): implementa a consulta dos usuários pelo SpringSecurity

--- src/main/java/com/fatec/itu/agendasalas/services/AuthService.java
+++ src/main/java/com/fatec/itu/agendasalas/services/AuthService.java
@@ -1,5 +1,8 @@
 package com.fatec.itu.agendasalas.services;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -9,24 +12,33 @@ import com.fatec.itu.agendasalas.entity.Usuario;
 import com.fatec.itu.agendasalas.repositories.UsuarioRepository;
 
 @Service
-public class AuthService {
+public class AuthService implements UserDetailsService{
 
     @Autowired
     private UsuarioRepository usuarioRepository;
     @Autowired
     private PasswordEncoder cryptPasswordEncoder;
 
-    public UsuarioAuthenticationResponseDTO login(UsuarioAuthenticationDTO usuarioAuthDTO){
-        Usuario user = usuarioRepository.findByLogin(usuarioAuthDTO.getLogin());
+
+    @Autowired
+    UsuarioRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException{
+        return repository.findByLogin(login);
+    }    
+
+    // public UsuarioAuthenticationResponseDTO login(UsuarioAuthenticationDTO usuarioAuthDTO){
+    //     Usuario user = usuarioRepository.findByLogin(usuarioAuthDTO.getLogin());
         
-        //autenticação bem vagabunda, mas só pra dar tempo de ter um login no sistema
-        //a autenticação certa usa o JWT Token
-        if(user!=null && cryptPasswordEncoder.matches(usuarioAuthDTO.getSenha(), user.getSenha())){
-            String cargoNome = (user.getCargo() != null) ? user.getCargo().getNome() : null;
-            return new UsuarioAuthenticationResponseDTO(user.getNome(), user.getId(), cargoNome);
-        }
-        throw new RuntimeException("Erro ao validar senha");
-    }
+    //     //autenticação bem vagabunda, mas só pra dar tempo de ter um login no sistema
+    //     //a autenticação certa usa o JWT Token
+    //     if(user!=null && cryptPasswordEncoder.matches(usuarioAuthDTO.getSenha(), user.getSenha())){
+    //         String cargoNome = (user.getCargo() != null) ? user.getCargo().getNome() : null;
+    //         return new UsuarioAuthenticationResponseDTO(user.getNome(), user.getId(), cargoNome);
+    //     }
+    //     throw new RuntimeException("Erro ao validar senha");
+    // }
 
  
 }
