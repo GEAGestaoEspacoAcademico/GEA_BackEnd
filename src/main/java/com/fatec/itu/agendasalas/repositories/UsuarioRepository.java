ad8e67b0692a24c5b148ef334689d2d196ff1518
Henrique A. R. <henrique4r.it@gmail.com>
2025-10-22 23:43:10 -0300

    feat(repository): implementa uma consulta de usuário por login com um retorno do tipo UserDetails

--- src/main/java/com/fatec/itu/agendasalas/repositories/UsuarioRepository.java
+++ src/main/java/com/fatec/itu/agendasalas/repositories/UsuarioRepository.java
@@ -1,5 +1,6 @@
 package com.fatec.itu.agendasalas.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 import com.fatec.itu.agendasalas.entity.Usuario;
@@ -9,7 +10,7 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 
     boolean existsByEmailAndIdNot(String email, Long id);
 
-    public Usuario findByLogin(String login);
+    UserDetails findByLogin(String login);
     
 
     
