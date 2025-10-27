324ec7085293166f2264cc0b19d71ac55dcb0379
LucasMorais42 <lucasmorais2214@gmail.com>
2025-10-27 11:24:54 -0300

    sparkles feature(JanelasHorario): Envio das DTOs de Janelas de Horario

---
+++ src/main/java/com/fatec/itu/agendasalas/dto/JanelasHorarioCreationDTO.java
@@ -0,0 +1,7 @@
+package com.fatec.itu.agendasalas.dto;
+
+import java.time.LocalTime;
+
+public record JanelasHorarioCreationDTO(LocalTime horaInicio, LocalTime horaFim){
+    
+}
---
+++ src/main/java/com/fatec/itu/agendasalas/dto/JanelasHorarioUpdateDTO.java
@@ -0,0 +1,7 @@
+package com.fatec.itu.agendasalas.dto;
+
+import java.time.LocalTime;
+
+public record JanelasHorarioUpdateDTO (LocalTime horaInicio, LocalTime horaFim) {
+
+}
