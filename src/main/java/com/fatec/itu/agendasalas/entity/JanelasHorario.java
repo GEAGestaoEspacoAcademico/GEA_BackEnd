3054b5bcab2fddfb30daabbdedff8e0642e08f42
LucasMorais42 <lucasmorais2214@gmail.com>
2025-10-27 11:17:06 -0300

    :sparkles: feature (JanelasHorario): Envio da entity janelasHorario

---
+++ src/main/java/com/fatec/itu/agendasalas/entity/JanelasHorario.java
@@ -0,0 +1,34 @@
+package com.fatec.itu.agendasalas.entity;
+
+import java.time.LocalTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name="JANELAS_HORARIO")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class JanelasHorario {
+    private static final long serialVersionUID = 1L;
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "hora_inicio", nullable=false)
+    private LocalTime horaInicio;
+
+    @Column(name = "hora_fim", nullable=false)
+    private LocalTime horaFim;
+}
