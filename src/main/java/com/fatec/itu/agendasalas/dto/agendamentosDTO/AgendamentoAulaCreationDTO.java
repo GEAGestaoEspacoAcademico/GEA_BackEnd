a28d99154538dfcee9db93a115755af5ddd8b395
LucasMorais42 <lucasmorais2214@gmail.com>
2025-10-27 12:53:51 -0300

    :recycle: refactor(agendamento): Ajeitando os service e controllers para ficar no padrão DTO + record

--- src/main/java/com/fatec/itu/agendasalas/dto/agendamentosDTO/AgendamentoAulaCreationDTO.java
+++ src/main/java/com/fatec/itu/agendasalas/dto/agendamentosDTO/AgendamentoAulaCreationDTO.java
@@ -3,36 +3,23 @@ package com.fatec.itu.agendasalas.dto.agendamentosDTO;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
-@Getter
-@Setter
-@NoArgsConstructor
-public class AgendamentoAulaCreationDTO {
+
+
+public record AgendamentoAulaCreationDTO (
+    @NotNull Long usuarioId,
+    @NotNull Long salaId,
+    @NotNull Long disciplinaId,
+    @NotNull LocalDate dataInicio,
+    @NotNull LocalDate dataFim,
+    @NotBlank String diaDaSemana,
+    @NotNull LocalTime horaInicio,
+    @NotNull LocalTime horaFim,
+    @NotBlank String tipo){}
+
+
     
-    private Long usuarioId;
-    private Long salaId;
-    private Integer disciplinaId;
-    private LocalDate dataInicio;
-    private LocalDate dataFim;
-    private String diaDaSemana;
-    private LocalTime horaInicio;
-    private LocalTime horaFim;
-    private String tipo;
 
-    public AgendamentoAulaCreationDTO(Long usuarioId, Long salaId, Integer disciplinaId, 
-                                       LocalDate dataInicio, LocalDate dataFim, String diaDaSemana,
-                                       LocalTime horaInicio, LocalTime horaFim, String tipo) {
-        this.usuarioId = usuarioId;
-        this.salaId = salaId;
-        this.disciplinaId = disciplinaId;
-        this.dataInicio = dataInicio;
-        this.dataFim = dataFim;
-        this.diaDaSemana = diaDaSemana;
-        this.horaInicio = horaInicio;
-        this.horaFim = horaFim;
-        this.tipo = tipo;
-    }
-}
+
