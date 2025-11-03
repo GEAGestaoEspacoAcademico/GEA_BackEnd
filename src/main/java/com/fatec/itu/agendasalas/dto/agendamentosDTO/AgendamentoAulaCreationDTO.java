57fb7950a768e663f95036a2d732357a9cacf6d9
Lucas Morais <lucasmorais2214@gmail.com>
2025-11-03 09:43:42 -0300

    Update AgendamentoAulaCreationDTO.java
    Evitando erros

--- src/main/java/com/fatec/itu/agendasalas/dto/agendamentosDTO/AgendamentoAulaCreationDTO.java
+++ src/main/java/com/fatec/itu/agendasalas/dto/agendamentosDTO/AgendamentoAulaCreationDTO.java
@@ -1,16 +1,21 @@
 package com.fatec.itu.agendasalas.dto.agendamentosDTO;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
 
-public record AgendamentoAulaCreationDTO(
-        Long usuarioId,
-        Long salaId,
-        Integer disciplinaId,
-        LocalDate dataInicio,
-        LocalDate dataFim,
-        String diaDaSemana,
-        LocalTime horaInicio,
-        LocalTime horaFim,
-        String tipo) {
-}
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+
+
+public record AgendamentoAulaCreationDTO (
+    @NotNull Long usuarioId,
+    @NotNull Long salaId,
+    @NotNull Long disciplinaId,
+    @NotNull LocalDate dataInicio,
+    @NotNull LocalDate dataFim,
+    @NotBlank String diaDaSemana,
+    @NotNull Long janelasHorarioId,
+    @NotBlank String tipo){}
+
+
+    
