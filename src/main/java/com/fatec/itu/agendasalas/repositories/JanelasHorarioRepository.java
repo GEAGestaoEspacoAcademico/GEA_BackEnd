9bc7d1ee4bb9087b0b40344079e5cc07f6085057
Walinson Pereira <walinsonoliveirapereira@gmail.com>
2025-11-05 18:47:47 -0300

    feat: Cria endpoint para mostrar horarios disponiveis de agendamentos

--- src/main/java/com/fatec/itu/agendasalas/repositories/JanelasHorarioRepository.java
+++ src/main/java/com/fatec/itu/agendasalas/repositories/JanelasHorarioRepository.java
@@ -1,11 +1,23 @@
 package com.fatec.itu.agendasalas.repositories;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.fatec.itu.agendasalas.entity.JanelasHorario;
 
 @Repository
 public interface JanelasHorarioRepository extends JpaRepository<JanelasHorario, Long> {
+    @Query("""
+    SELECT j FROM JanelasHorario j
+    WHERE j.id NOT IN (
+        SELECT a.janelasHorario.id FROM Agendamento a
+        WHERE DATE(a.dataInicio) = :data
+    )
+    """)
+    List<JanelasHorario> findDisponiveisPorData(LocalDate data);
 
 }
