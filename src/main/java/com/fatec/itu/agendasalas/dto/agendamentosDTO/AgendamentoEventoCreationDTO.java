4dcacb12751c1ea27c9a64d2634e35d277e13fea
LucasMorais42 <lucasmorais2214@gmail.com>
2025-11-11 17:56:21 -0300

    :sparkles: feat: criacao dto agendamentoEventoCreation

--- src/main/java/com/fatec/itu/agendasalas/dto/agendamentosDTO/AgendamentoEventoCreationDTO.java
+++ src/main/java/com/fatec/itu/agendasalas/dto/agendamentosDTO/AgendamentoEventoCreationDTO.java
@@ -1,10 +1,12 @@
 package com.fatec.itu.agendasalas.dto.agendamentosDTO;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.Date;
 import java.util.List;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record AgendamentoEventoCreationDTO(
 @NotBlank(message = "O nome do evento não pode ser nulo")    
@@ -13,8 +15,17 @@ String nomeEvento,
 @NotBlank(message = "O local do evento não pode ser nulo")   
 String local,
 
+@NotNull(message = "O dia de inicio de agendamento de um evento não pode ser nulo")
 LocalDateTime diaInicio,
+
+@NotNull(message = "O dia de fim de agendamento de um evento não pode ser nulo")
 LocalDateTime diaFim,
+
+@NotNull(message = "O horario de inicio de agendamento de um evento não pode ser nulo")
+LocalTime horaInicio,
+
+@NotNull(message = "O horario de fim de agendamento de um evento não pode ser nulo")
+LocalTime horafim
 
 
 
