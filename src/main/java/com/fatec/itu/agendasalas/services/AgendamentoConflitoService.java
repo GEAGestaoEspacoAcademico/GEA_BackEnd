de020bb1ec6ca5aea6b087789428e0502e1714b3
LucasMorais42 <lucasmorais2214@gmail.com>
2025-11-14 12:07:01 -0300

    :bug: fix: ajustando com conflitoService em aulas

--- src/main/java/com/fatec/itu/agendasalas/services/AgendamentoConflitoService.java
+++ src/main/java/com/fatec/itu/agendasalas/services/AgendamentoConflitoService.java
@@ -1,14 +1,10 @@
 package com.fatec.itu.agendasalas.services;
 
 import java.time.LocalDate;
-import java.util.List;
 
 import org.springframework.stereotype.Service;
 
 import com.fatec.itu.agendasalas.entity.AgendamentoAula;
-import com.fatec.itu.agendasalas.entity.AgendamentoEvento;
-import com.fatec.itu.agendasalas.entity.JanelasHorario;
-import com.fatec.itu.agendasalas.entity.Sala;
 import com.fatec.itu.agendasalas.repositories.AgendamentoAulaRepository;
 import com.fatec.itu.agendasalas.repositories.AgendamentoEventoRepository;
 import com.fatec.itu.agendasalas.repositories.AgendamentoRepository;
@@ -30,16 +26,16 @@ public class AgendamentoConflitoService {
     }
 
     
-    public boolean existeEventoNoHorario(Sala sala, LocalDate data, JanelasHorario janela){
-        return agendamentoEventoRepository.existsBySalaIdAndDataAndJanelasHorarioId(sala.getId(), data, janela.getId());
+    public boolean existeEventoNoHorario(Long sala, LocalDate data, Long janela){
+        return agendamentoEventoRepository.existsBySalaIdAndDataAndJanelasHorarioId(sala, data, janela);
     }
 
-    public boolean existeAgendamentoNoHorario(Sala sala, LocalDate data, JanelasHorario janela){
-        return agendamentoRepository.existsBySalaIdAndDataAndJanelasHorarioId(sala.getId(), data, janela.getId());
+    public boolean existeAgendamentoNoHorario(Long sala, LocalDate data, Long janela){
+        return agendamentoRepository.existsBySalaIdAndDataAndJanelasHorarioId(sala, data, janela);
     }
 
-    public AgendamentoAula filtrarAulasConflitantes(Sala sala, LocalDate data, JanelasHorario janela){
-        return agendamentoAulaRepository.findBySalaIdAndDataAndJanelasHorarioId(sala.getId(), data, janela.getId());
+    public AgendamentoAula filtrarAulasConflitantes(Long sala, LocalDate data, Long janela){
+        return agendamentoAulaRepository.findBySalaIdAndDataAndJanelasHorarioId(sala, data, janela);
     }
 
 
