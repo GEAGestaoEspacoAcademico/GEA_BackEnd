b6e2a03f70ac1e888281b2058a438a8a400f5b5e
LucasMorais42 <lucasmorais2214@gmail.com>
2025-11-15 13:02:26 -0300

    ajeitando para nao alterar nada de aula por enquanto

--- src/main/java/com/fatec/itu/agendasalas/dto/agendamentosDTO/AgendamentoAulaCreationDTO.java
+++ src/main/java/com/fatec/itu/agendasalas/dto/agendamentosDTO/AgendamentoAulaCreationDTO.java
@@ -1,5 +1,19 @@
 package com.fatec.itu.agendasalas.dto.agendamentosDTO;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
-public record AgendamentoAulaCreationDTO() {
+import jakarta.validation.constraints.NotNull;
 
-}
+public record AgendamentoAulaCreationDTO(
+
+
+
+    @NotNull Long usuarioId,
+    @NotNull Long salaId,
+    @NotNull Long disciplinaId,
+    @NotNull LocalDate data,
+    @NotNull LocalTime horaInicio,
+    @NotNull LocalTime horaFim
+    
+)
+{}
