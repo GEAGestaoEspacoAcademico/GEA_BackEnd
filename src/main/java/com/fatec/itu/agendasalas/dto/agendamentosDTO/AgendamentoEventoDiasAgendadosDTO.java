df500187a9366375847f2fca5a4c61bf1333f980
LucasMorais42 <lucasmorais2214@gmail.com>
2025-11-16 11:32:14 -0300

    :bug: fix: adaptando dto para lista de dias

--- src/main/java/com/fatec/itu/agendasalas/dto/agendamentosDTO/AgendamentoEventoDiasAgendadosDTO.java
+++ src/main/java/com/fatec/itu/agendasalas/dto/agendamentosDTO/AgendamentoEventoDiasAgendadosDTO.java
@@ -1,10 +1,7 @@
 package com.fatec.itu.agendasalas.dto.agendamentosDTO;
 
-import java.time.LocalDate;
-import java.util.List;
+import java.time.LocalDateTime;
 
-public record AgendamentoEventoDiasAgendadosDTO(
-    LocalDate dia,
-    List<Long> janelasHorarioId
-) {   
+public record AgendamentoEventoDiasAgendadosDTO() {
+    LocalDateTime dia;
 }
