6b124e247ad6f353a733bcfb6d7204e7ed61d4d3
LucasMorais42 <lucasmorais2214@gmail.com>
2025-11-17 17:52:05 -0300

    :recycle: refactor: arrumando dto

--- src/main/java/com/fatec/itu/agendasalas/dto/eventosDTO/EventoCreationDTO.java
+++ src/main/java/com/fatec/itu/agendasalas/dto/eventosDTO/EventoCreationDTO.java
@@ -1,7 +1,12 @@
 package com.fatec.itu.agendasalas.dto.eventosDTO;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record EventoCreationDTO(
-    long eventoId,
+
+    @NotBlank(message="O nome do evento não pode ser vazio")
     String nomeEvento,
+
+    @NotBlank(message="A descrição do evento não pode ser nula")
     String descricaoEvento
 ){}
