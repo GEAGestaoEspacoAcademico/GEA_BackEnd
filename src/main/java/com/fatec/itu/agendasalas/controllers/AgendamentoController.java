79e1120001a1d2a950a34826def37ff742471ba5
Walinson Pereira <walinsonoliveirapereira@gmail.com>
2025-11-18 19:54:15 -0300

    feat: CancelarAgendamentos

--- src/main/java/com/fatec/itu/agendasalas/controllers/AgendamentoController.java
+++ src/main/java/com/fatec/itu/agendasalas/controllers/AgendamentoController.java
@@ -3,19 +3,27 @@ package com.fatec.itu.agendasalas.controllers;
 import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fatec.itu.agendasalas.dto.agendamentosDTO.AgendamentoCanceladoRequestDTO;
+import com.fatec.itu.agendasalas.dto.agendamentosDTO.AgendamentoCanceladoResponseDTO;
 import com.fatec.itu.agendasalas.dto.agendamentosDTO.AgendamentoNotificacaoDisciplinaDTO;
+import com.fatec.itu.agendasalas.entity.Usuario;
+import com.fatec.itu.agendasalas.repositories.UsuarioRepository;
 import com.fatec.itu.agendasalas.services.AgendamentoService;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.persistence.EntityNotFoundException;
 
 @CrossOrigin
 @RestController
@@ -24,6 +32,9 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 public class AgendamentoController {
 
     private final AgendamentoService agendamentoService;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
 
     public AgendamentoController(AgendamentoService agendamentoService) {
         this.agendamentoService = agendamentoService;
@@ -46,5 +57,42 @@ public class AgendamentoController {
                     return ResponseEntity.noContent().build();
                 }
                 return ResponseEntity.ok(agendamentos);
+    }
+
+    @PutMapping("/{id}/cancelar")
+    public ResponseEntity<?> cancelarAgendamento(
+        @PathVariable Long id,
+        @RequestBody AgendamentoCanceladoRequestDTO request) {
+
+            if (request.motivoCancelamento() == null || request.motivoCancelamento().trim().isEmpty()) {
+                return ResponseEntity.badRequest().body("O motivo do cancelamento é obrigatório.");
+            }
+
+            if (request.usuarioId() == null) {
+                return ResponseEntity.badRequest().body("O ID do usuário é obrigatório.");
+            }
+
+            try {
+        
+                Usuario usuarioCancelador = usuarioRepository.findById(request.usuarioId())
+                    .orElseThrow(() -> new EntityNotFoundException("Usuário não encontrado."));
+
+                AgendamentoCanceladoResponseDTO responseDTO =
+                agendamentoService.cancelarAgendamento(id, usuarioCancelador, request);
+
+                return ResponseEntity.ok(responseDTO);
+
+            } catch (EntityNotFoundException e) {
+                return ResponseEntity.status(404).body(e.getMessage());
+
+            } catch (IllegalArgumentException e) {
+                return ResponseEntity.badRequest().body(e.getMessage());
+
+            } catch (IllegalStateException e) {
+                return ResponseEntity.status(409).body(e.getMessage());
+
+            } catch (Exception e) {
+                return ResponseEntity.internalServerError().body("Erro inesperado no cancelamento: " + e.getMessage());
+            }
     }
 }
