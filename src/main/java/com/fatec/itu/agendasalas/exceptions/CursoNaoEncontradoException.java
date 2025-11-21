6a4675f28482fbb07a72347b8cf72f3ab5d44fb8
Walinson Pereira <walinsonoliveirapereira@gmail.com>
2025-11-21 12:39:49 -0300

    feat: Cria o endpoint para listar as Disciplinas do Curso pelo ID do Curso

---
+++ src/main/java/com/fatec/itu/agendasalas/exceptions/CursoNaoEncontradoException.java
@@ -0,0 +1,7 @@
+package com.fatec.itu.agendasalas.exceptions;
+
+public class CursoNaoEncontradoException extends RuntimeException {
+    public CursoNaoEncontradoException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/fatec/itu/agendasalas/repositories/DisciplinaRepository.java
+++ src/main/java/com/fatec/itu/agendasalas/repositories/DisciplinaRepository.java
@@ -1,11 +1,15 @@
 package com.fatec.itu.agendasalas.repositories;
 
 import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import com.fatec.itu.agendasalas.entity.Disciplina;
 
 @Repository
 public interface DisciplinaRepository extends JpaRepository<Disciplina, Long> {
   public List<Disciplina> findByProfessorId(Long professorId);
+
+  public List<Disciplina> findByCursoId(Long cursoId);
 }
