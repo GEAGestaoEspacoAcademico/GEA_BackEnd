de8721fbdd073d3dcc6df60013a4a947ff9ef704
LucasMorais42 <lucasmorais2214@gmail.com>
2025-11-22 11:57:18 -0300

    :sparkles: feat: adição de novos metodos no service

---
+++ src/main/java/com/fatec/itu/agendasalas/exceptions/MatriculaDuplicadaSecretariaException.java
@@ -0,0 +1,5 @@
+package com.fatec.itu.agendasalas.exceptions;
+
+public class MatriculaDuplicadaSecretariaException {
+
+}
--- src/main/java/com/fatec/itu/agendasalas/services/SecretariaService.java
+++ src/main/java/com/fatec/itu/agendasalas/services/SecretariaService.java
@@ -5,9 +5,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.fatec.itu.agendasalas.dto.secretariaDTO.SecretariaCreationDTO;
 import com.fatec.itu.agendasalas.dto.secretariaDTO.SecretariaResponseDTO;
+import com.fatec.itu.agendasalas.entity.Cargo;
 import com.fatec.itu.agendasalas.entity.Secretaria;
+import com.fatec.itu.agendasalas.exceptions.CargoNaoEncontradoException;
+import com.fatec.itu.agendasalas.exceptions.EmailJaCadastradoException;
+import com.fatec.itu.agendasalas.exceptions.MatriculaDuplicadaSecretariaException;
 import com.fatec.itu.agendasalas.exceptions.SecretariaNaoEncontradoException;
+import com.fatec.itu.agendasalas.repositories.CargoRepository;
 import com.fatec.itu.agendasalas.repositories.SecretariaRepository;
 
 @Service
@@ -15,6 +21,12 @@ public class SecretariaService {
 
     @Autowired
     private SecretariaRepository secretariaRepository;
+
+    @Autowired
+    private CargoRepository cargoRepository;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
 
     public List<SecretariaResponseDTO> listarSecretarios() {
         return secretariaRepository.findAll().stream()
@@ -28,12 +40,64 @@ public class SecretariaService {
         return converterParaResponseDTO(secretaria);
     }
 
-    
+    public SecretariaResponseDTO cadastrarSecretaria(SecretariaCreationDTO dto) {
+        // Aqui você pode validar se já existe secretaria com a mesma matricula
+        if(secretariaRepository.existsByMatricula(dto.matricula())) {
+            throw new MatriculaDuplicadaSecretariaException(dto.matricula());
+        }
+        if(usuarioRepository.existsByEmail(dto.email())){
+            throw new EmailJaCadastradoException(dto.email());
+        }
+
+        Secretaria secretaria = new Secretaria();
+        secretaria.setNome(dto.nome());
+        secretaria.setEmail(dto.email());
+        secretaria.setMatricula(dto.matricula());
+
+        Cargo cargo = cargoRepository.findByNome("SECRETARIA")
+            .orElseThrow(() -> new CargoNaoEncontradoException("SECRETARIA"));
+        secretaria.setCargo(cargo);
+
+        secretariaRepository.save(secretaria);
+
+        return new SecretariaResponseDTO(
+            secretaria.getId(),
+            secretaria.getNome(),
+            secretaria.getEmail(),
+            secretaria.getMatricula(),
+            secretaria.getCargo().getId()
+        );
+    }
+
+    public SecretariaResponseDTO atualizarSecretaria(Long id, SecretariaCreationDTO dto) {
+        Secretaria secretaria = secretariaRepository.findById(id)
+            .orElseThrow(() -> new SecretariaNaoEncontradoException(id));
+
+        secretaria.setNome(dto.nome());
+        secretaria.setEmail(dto.email());
+        secretaria.setMatricula(dto.matricula());
+
+        secretariaRepository.save(secretaria);
+
+        return new SecretariaResponseDTO(
+            secretaria.getId(),
+            secretaria.getNome(),
+            secretaria.getEmail(),
+            secretaria.getMatricula(),
+            secretaria.getCargo().getId()
+        );
+    }
 
     private SecretariaResponseDTO converterParaResponseDTO(Secretaria secretaria){
         return new SecretariaResponseDTO(secretaria.getId(), secretaria.getNome(), secretaria.getEmail(), secretaria.getMatricula(), secretaria.getCargo().getId());
     }
 
+    public void deletarSecretaria(Long id) {
+        Secretaria secretaria = secretariaRepository.findById(id)
+            .orElseThrow(() -> new SecretariaNaoEncontradoException(id));
+
+        secretariaRepository.delete(secretaria);
+    }
 
     
 }
