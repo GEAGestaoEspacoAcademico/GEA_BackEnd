172f10c2a6a275c8a8f8febd1faf8a59fddbcb52
Lucas Morais <lucasmorais2214@gmail.com>
2025-11-22 12:44:49 -0300

    Add constructor for CursoNaoEncontradoException by ID

--- src/main/java/com/fatec/itu/agendasalas/exceptions/CursoNaoEncontradoException.java
+++ src/main/java/com/fatec/itu/agendasalas/exceptions/CursoNaoEncontradoException.java
@@ -2,6 +2,10 @@ package com.fatec.itu.agendasalas.exceptions;
 
 public class CursoNaoEncontradoException extends RuntimeException{
 
+    public CursoNaoEncontradoException(Long id){
+        super("Não foi encontrado curso com o id: " + id);
+    }
+    
     public CursoNaoEncontradoException(String sigla){
         super("Não foi encontrado curso com a sigla: " + sigla);
     }
