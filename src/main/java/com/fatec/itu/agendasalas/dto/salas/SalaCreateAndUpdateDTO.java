b6e415249351857bff19c3aea659fd36bfcfda66
Henrique A. R. <henrique4r.it@gmail.com>
2025-11-23 11:49:00 -0300

    refactor: renomeia 'andar' para 'piso' em todo o código para evitar mudanças drásticas no front-end

--- src/main/java/com/fatec/itu/agendasalas/dto/salas/SalaCreateAndUpdateDTO.java
+++ src/main/java/com/fatec/itu/agendasalas/dto/salas/SalaCreateAndUpdateDTO.java
@@ -2,7 +2,7 @@ package com.fatec.itu.agendasalas.dto.salas;
 
 public record SalaCreateAndUpdateDTO(
         Long tipoSalaId,
-        Long andarId,
+        Long pisoId,
         String salaNome, 
         Integer salaCapacidade, 
         Boolean disponibilidade,
