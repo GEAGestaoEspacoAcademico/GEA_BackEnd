c3a50a06e26fa83256ea0fad5500e2aa5e4a9675
LucasMorais42 <lucasmorais2214@gmail.com>
2025-11-23 13:37:46 -0300

    :bug: fix: arrumando construtor do passwordresettoken

--- src/main/java/com/fatec/itu/agendasalas/entity/PasswordResetToken.java
+++ src/main/java/com/fatec/itu/agendasalas/entity/PasswordResetToken.java
@@ -1,5 +1,40 @@
 package com.fatec.itu.agendasalas.entity;
 
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
 public class PasswordResetToken {
+    private static final int EXPIRATION = 60 * 24;
+ 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+ 
+    private String token;
+ 
+    @OneToOne(targetEntity = Usuario.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "user_id")
+    private Usuario usuario;
+ 
+    private Date dataExpiracao;
+
+    public PasswordResetToken(Usuario usuario, String token){
+        this.token = token;
+        this.usuario = usuario;
+    }
 
 }
